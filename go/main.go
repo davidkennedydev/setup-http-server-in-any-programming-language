f4b7f5ef63c2ddb12b9d5f521ae007d3c0a000c2
David Kennedy S. Araújo <software@davidkennedy.dev>
2023-08-30 14:26:44 -0300

    Setup minimal Go HTTP server

---
+++ go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", HelloHandler)
+  http.ListenAndServe(":8080", nil)
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, GoLang!")
+}
+
+
+
+
